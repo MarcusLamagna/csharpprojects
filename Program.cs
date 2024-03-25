8ed4a15a94540f7e0b8176c8510b195ba6ff6f49
Marcus Lamagna <marcus@mlamagna.com.br>
2024-03-25 11:11:24 -0300

    Declarando nova matriz - Atribuir valores a elementos de uma matriz

--- Program.cs
+++ Program.cs
@@ -1,28 +1,23 @@
-﻿//Introdução às noções básicas de array
-/*
-*
-*
-*
-*/
+﻿//Declarando nova matriz - Atribuir valores a elementos de uma matriz
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+//string[] fraudulentOrderIDs = new string[3];
 
-if (daysUntilExpiration == 1){
-    Console.WriteLine("Your subscription expires within a day!");
-}
-else if (daysUntilExpiration == 1){
-    Console.WriteLine("Your subscription expires within a day");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5){
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <=10){
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-if (discountPercentage > 0){
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
+//fraudulentOrderIDs[0] = "A123";
+//fraudulentOrderIDs[1] = "B456";
+//fraudulentOrderIDs[2] = "C789";
+
+//Inicializar uma matriz
+
+string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+//REatribuir o vaor de uma matriz
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
+
+//No final do arquivo de código, para relatar o número de pedidos fraudulentos, digite o seguinte código:
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
