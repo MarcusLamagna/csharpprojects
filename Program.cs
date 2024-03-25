58da61b636151477e7c315c8ec1220174a315720
Marcus Lamagna <marcus@mlamagna.com.br>
2024-03-25 10:34:35 -0300

    Introdução às noções básicas de array

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
-﻿//Criei uma classe e armezenei uma referência ao objeto em uma variável chamada dice.
-/*Regra 1: Seu código deve exibir apenas uma mensagem.
-*Regra 2: Se a assinatura do usuário expirar em 10 dias ou menos, exiba a mensagem:
-*Regra 4: Se a assinatura do usuário expirar em um dia, exiba as mensagens:
-*Regra 5: Se a assinatura do usuário expirou, exiba a mensagem:
-*Regra 6: Se a assinatura do usuário não expirar em 10 dias ou menos, não exiba nada.
+﻿//Introdução às noções básicas de array
+/*
+*
+*
+*
 */
 
 Random random = new Random();
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
-
-// Your code goes here
-//Crie uma instrução que exiba uma if-else mensagem sobre quando a assinatura expirará.
-//Crie uma declaração separada if que exiba uma oferta de desconto.
 
 if (daysUntilExpiration == 1){
     Console.WriteLine("Your subscription expires within a day!");
